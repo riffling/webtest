6d929bf5791f289c49646a2f2bc88effbcb65e1a
Gong Yongjie <yong-jie.gong@hp.com>
2015-11-20 16:42:48 +0800

    add test tool kit

---
+++ h10_mvn/dapp/src/main/java/org/mvntest/dapp/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.mvntest.dapp;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World:"+ java.util.Arrays.asList(args));
+    }
+
+}
