edee2826ad335157407b258c3dc6b060e4c0f19e
Gong Yongjie <yong-jie.gong@hp.com>
2015-11-24 13:45:46 +0800

    asdf

---
+++ h10_mvn/wapp/src/main/java/org/mvntest/wapp/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.mvntest.wapp;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World:"+ java.util.Arrays.asList(args));
+    }
+
+}
